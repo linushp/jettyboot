9c0ba353797fc3d7528ca1207d7c53871f4ad921
luan <master@ubibi.cn>
2018-05-12 13:59:48 +0800

    DWR支持任意数组形式的参数

--- src/main/java/cn/ubibi/jettyboot/framework/commons/CastTypeUtils.java
+++ src/main/java/cn/ubibi/jettyboot/framework/commons/CastTypeUtils.java
@@ -16,20 +16,19 @@ public class CastTypeUtils {
     }
 
 
-    // 基本数据类型转换
+    // 基本数据类型转换,返回一个  Array.newInstance
     public static Object toTypeArrayOf(Collection jsonArray, Class elementType) throws Exception {
         if (jsonArray == null) {
             return null;
         }
-
-        Object result = Array.newInstance(elementType, jsonArray.size());
+        Object array_result = Array.newInstance(elementType, jsonArray.size());
         int index = 0;
         for (Object obj : jsonArray) {
             Object obj2 = toTypeOf(obj, elementType);
-            Array.set(result, index, obj2);
+            Array.set(array_result, index, obj2);
             index++;
         }
-        return result;
+        return array_result;
 
     }
 
@@ -39,14 +38,14 @@ public class CastTypeUtils {
         if (jsonArray == null) {
             return null;
         }
-        Object result = Array.newInstance(elementType, jsonArray.size());
+        Object array_result = Array.newInstance(elementType, jsonArray.size());
         int index = 0;
         for (Object obj : jsonArray) {
             Object obj2 = jsonObjectToJavaObject(obj, elementType);
-            Array.set(result, index, obj2);
+            Array.set(array_result, index, obj2);
             index++;
         }
-        return result;
+        return array_result;
     }
 
 
